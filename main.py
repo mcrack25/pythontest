147d2f67199a5642410113204eb22615e53f2682
mcrack25 <mcrack@yandex.ru>
2020-07-07 20:28:35 +0300

    abc

---
+++ main.py
@@ -0,0 +1,13 @@
+import time
+import os
+import subprocess
+
+ROOT_DIR = os.getcwd()
+
+
+if __name__ == '__main__':
+    stop = False
+    while stop == False:
+        print('ok')
+
+        time.sleep(1)
